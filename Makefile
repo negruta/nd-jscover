9ee4262e558774e830403866714bb0784501f0cc
Dumitru Negruta <negruta@adobe.com>
2014-11-26 18:36:27 +0200

    push changes

---
+++ Makefile
@@ -0,0 +1,24 @@
+TESTS = test/*.test.js
+REPORTER = spec
+TIMEOUT = 5000
+GLOBALS = _*jscoverage
+
+test:
+	@NODE_ENV=test ./node_modules/mocha/bin/mocha \
+		--reporter $(REPORTER) \
+		--globals $(GLOBALS) \
+		--timeout $(TIMEOUT) \
+		$(TESTS)
+
+test-cov:
+	@$(MAKE) lib-cov
+	@JSCOVER_COV=1 $(MAKE) test
+	@JSCOVER_COV=1 $(MAKE) test REPORTER=html-cov > coverage.html
+
+all-cov:
+	@node ./bin/jscover --exclude=lib-cov --exclude=test --exclude=bin . $@
+
+lib-cov:
+	@node ./bin/jscover lib $@
+
+.PHONY: test-cov test lib-cov all-cov
---
+++ test/jscover.test.js
@@ -0,0 +1,73 @@
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var jscover = require('../');
+var should = require('should');
+var path = require('path');
+var fs = require('fs');
+var fse = require('fs-extra');
+fs.existsSync = fs.existsSync || path.existsSync;
+
+
+describe('jscover.test.js', function () {
+  var source = path.join(__dirname, 'lib');
+  var target = path.join(__dirname, 'lib-cov');
+
+  beforeEach(function () {
+    fse.removeSync(target);
+  });
+
+  it('should coverage lib to lib-cov', function (done) {
+    should.ok(!fs.existsSync(target));
+    jscover(source, target, null, function (err, stdout) {
+      should.exist(err);
+      should.exist(stdout);
+      should.ok(fs.existsSync(target));
+      done();
+    });
+  });
+
+  it('should coverage lib to lib-cov with --exclude=subdir', function (done) {
+    should.ok(!fs.existsSync(target));
+    should.ok(!fs.existsSync(path.join(target, 'subdir')));
+    jscover(source, target, ['--exclude=subdir'], function (err, stdout) {
+      should.exist(err);
+      should.exist(stdout);
+      should.ok(fs.existsSync(target));
+      should.ok(!fs.existsSync(path.join(target, 'subdir')));
+      done();
+    });
+  });
+
+  it('should return stdout when args missing', function (done) {
+    jscover('', null, {}, function (err) {
+      should.exist(err);
+      err.name.should.equal('JSCoverError');
+      err.message.trim().should.endWith("A minimum of 3 arguments is required");
+      done();
+    });
+  });
+
+  it('should return error when dir not exists', function (done) {
+    jscover('a', 'b', {}, function (err) {
+      should.exist(err);
+      err.name.should.equal('JSCoverError');
+      err.message.trim().should.endWith("Source directory 'a' is invalid");
+      done();
+    });
+  });
+
+  describe('utf8', function () {
+    it('should coverage no-ascii char success', function (done) {
+      should.ok(!fs.existsSync(target));
+      jscover(source, target, null, function (err, output) {
+        should.exist(err);
+        should.exist(output);
+        done();
+      });
+    });
+  });
+});
